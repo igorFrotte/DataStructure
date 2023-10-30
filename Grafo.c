e1a86294a093a024af1187fb3a4315235a484d01
igorFrotte <igorfrotte@gmail.com>
2023-10-29 21:29:14 -0300

    graph insert

--- Grafo.c
+++ Grafo.c
@@ -11,6 +11,56 @@ typedef struct list{
     struct list *next;
 }List;
 
+List** createGraph(int n){
+    List **g = (List**)malloc((n + 1) * sizeof(List*));
+    for(int i = 0; i <= n; i++) 
+        g[i] = NULL;
+    return g;
+}
+
+List *InseritList(List *list, int d, int c){
+    List *new = (List *) malloc(sizeof(List));
+    new->dest = d;
+    new->cost = c;
+    new->next = list;
+    return new;
+}
+
+void InsertEdge(List **g, int o, int d, int c){
+    g[o] = InsertList(g[o], d, c);
+}
+
+
+List *RemoveItem(List *list, int d){
+    if(list == NULL) 
+        return NULL;
+
+    List *prev = list;
+    List *aux = prev->next;
+
+    if(prev->dest == d){
+        list = aux;
+        free(prev);
+    } else {
+        while(aux != NULL && prev->dest != d){
+            prev = aux;
+            aux = aux->next;
+        }
+        if(aux != NULL){
+            prev->next = aux->next;
+            free(aux);
+        }
+    }
+    return list;
+}
+
+void RemoveEdge(List **g, int o, int d) {
+    g[o] = RemoveItem(g[o], d);  
+}
+
+
+
+
 
 
 
@@ -27,13 +77,21 @@ int mainMenu(){
 }
 
 int main(){
-    int option;
+    int option, n;
+    printf("\nDigite o número de vértices do Grafo.\n");
+    scanf("%d", &n);
+
+    List** graph = createGraph(n);
 
     while (option != 6){
-    
+
         option = mainMenu();
         if(option == 1){
-            
+            int orig, dest, cost;
+            printf("Digite a origem, o destino e o custo (Nesta ordem): \n");
+            scanf("%d %d %d", &orig, &dest, &cost);
+
+            InsertEdge(graph,  orig,  dest,  cost);
         }
         if(option == 2){
             
