f339ef2e9d3f8df7c03db9f36bf4636421ae600c
igorFrotte <igorfrotte@gmail.com>
2023-11-14 21:48:49 -0300

    starting bin file

--- Hashing.c
+++ Hashing.c
@@ -1,3 +1,27 @@
+#include <stdio.h>
+
+#define N 11
+
+typedef struct student {
+    int regist;
+    char name[50];
+    char course[50];
+} Student;
+
+typedef struct data {
+    Student stud;
+    int avail;
+} Data;
+
+void fStart(char *fName){
+    FILE *f = fopen(fName, "wb");
+    Data d;
+    d.avail = 1;
+    int i;
+    for(i = 0; i<N ; i++)
+        fwrite(&d, sizeof(Data), 1, f);
+    fclose(f);
+}
 
 
 
