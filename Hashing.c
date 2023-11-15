cf36d5d89516ceb781d30bebb5e01db2dab74fd2
igorFrotte <igorfrotte@gmail.com>
2023-11-14 21:21:39 -0300

    starting hashing

---
+++ Hashing.c
@@ -0,0 +1,33 @@
+
+
+
+int mainMenu(){
+    int option;
+    printf("\n1- Inserir um novo Aluno.");
+    printf("\n2- Imprimir informações do Aluno.");
+    printf("\n3- Imprimir a tabela de dispersão.");
+    printf("\n4- Sair\n");
+    scanf("%d", &option);
+    return option;
+}
+
+int main(){
+    int option;
+
+    while (option != 4){
+
+        option = mainMenu();
+        if(option == 1){
+           
+        }
+        if(option == 2){
+            
+        }
+        if(option == 3){
+            
+        }
+        if(option == 4){
+            
+        }
+    }
+}
